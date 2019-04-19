087958867c66f880b18a7afeac2bad3f018e86c5
weathon <tcs06358@163.com>
2019-04-19 14:26:40 -0500

    修复了使用前一定要先初始化的问题

--- script.js
+++ script.js
@@ -1,60 +1,52 @@
-main = function () {
-			var context = new AudioContext();
-			var fileInput = document.getElementById("fileInput");
+function upload() {
+    var context = new AudioContext();
+    var files = fileInput.files;
 
-			fileInput.onchange = function () {
-				var files = fileInput.files;
+    if (files.length == 0) return;
+    var reader = new FileReader();
 
-				if (files.length == 0) return;
-				var reader = new FileReader();
+    reader.onload = function (fileEvent) {
+        context.decodeAudioData(fileEvent.target.result, calcTempo);
+    }
 
-				reader.onload = function (fileEvent) {
-					context.decodeAudioData(fileEvent.target.result, calcTempo);
-				}
+    reader.readAsArrayBuffer(files[0]);
+}
 
-				reader.readAsArrayBuffer(files[0]);
-			}
-			var calcTempo = function (buffer) {
-				var audioData = [];
-				// Take the average of the two channels
-				if (buffer.numberOfChannels == 2) {
-					var channel1Data = buffer.getChannelData(0);
-					var channel2Data = buffer.getChannelData(1);
-					var length = channel1Data.length;
-					for (var i = 0; i < length; i++) {
-						audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
-					}
-				} else {
-					audioData = buffer.getChannelData(0);
-				}
-				mt = new MusicTempo(audioData);
-				
-				console.log("mt.tempo");
-				console.log(mt.tempo);
-				console.log("mt.beats");
-                console.log(String(mt.beats));
-                console.log("Audio Data");
-                console.log(audioData)//做了这么大半天居然没有想到直接输出mt，文档不全？没看DEMO~
-			}
+var calcTempo = function (buffer) {
+    var audioData = [];
+    // Take the average of the two channels
+    if (buffer.numberOfChannels == 2) {
+        var channel1Data = buffer.getChannelData(0);
+        var channel2Data = buffer.getChannelData(1);
+        var length = channel1Data.length;
+        for (var i = 0; i < length; i++) {
+            audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
         }
-        
+    } else {
+        audioData = buffer.getChannelData(0);
+    }
+    mt = new MusicTempo(audioData);
+
+    //做了这么大半天居然没有想到直接输出mt，文档不全？没看DEMO~
+    console.log(mt);
+}
 
 
-function play()
-{
-    document.getElementById("link").setAttribute("src",document.getElementById("fileInput").files)
+
+
+function play() {
+    document.getElementById("link").setAttribute("src", document.getElementById("fileInput").files)
     document.getElementById("myaudio").play();
     console.log(mt.beats.length);
-    for(let i=0;i<mt.beats.length;i++)
-    {
-        setTimeout(function(){
+    for (let i = 0; i < mt.beats.length; i++) {
+        setTimeout(function () {
             console.log(i)
-            document.getElementById("bar").setAttribute("style","background:#ffffff")
-            setTimeout(function(){
-                document.getElementById("bar").setAttribute("style","background:#000000")
-            },250)
-        },mt.beats[i]*1000)
+            document.getElementById("bar").setAttribute("style", "background:#ffffff")
+            setTimeout(function () {
+                document.getElementById("bar").setAttribute("style", "background:#000000")
+            }, 250)
+        }, mt.beats[i] * 1000)
     }
-        
+
 
 }
